30a596b03d49a8746c63e5b824501cf4d264ed49
novitasalsabila <novnovv10@gmail.com>
2023-02-18 21:33:01 +0700

    Add project files.

---
+++ KEDAI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEDAI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HAII KAMU PASTI LAGI LAPAR YAA");
+            Console.WriteLine("yaudah sini mampir duluu ");
+
+            Console.WriteLine("PILIH MENU:");
+            Console.WriteLine("Makanan:");
+            Console.WriteLine("1.Oseng Ayam mati");
+            Console.WriteLine("2.Babi guling");
+            Console.WriteLine("3.Tumis biawak");
+            Console.WriteLine("4.Tempe busuk");
+            Console.WriteLine("Minuman:");
+            Console.WriteLine("1.Es boba tai kambing");
+            Console.WriteLine("2.Es durian runtuh");
+            Console.WriteLine("3.Es kacang mentah");
+
+            Console.WriteLine("TERIMAKASIH SUDAH MAMPIR SINI");
+            Console.ReadKey();
+        
+          
+
+        }
+    }
+}
